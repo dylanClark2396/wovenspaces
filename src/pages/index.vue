a6a245669d226a470493c794b9ce128af9175e4c
Dylan Clark <dylanclark2396@gmail.com>
2025-06-23 00:06:56 -0500

    Add Nuxt project under src/

---
+++ src/pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <UContainer class="py-10">
+    <UCard>
+      <h1 class="text-3xl font-bold">Welcome to WovenSpaces 🌐</h1>
+      <p class="mt-2 text-gray-600">Nuxt 3 + Nuxt UI deployed via GitHub Pages.</p>
+    </UCard>
+  </UContainer>
+</template>
